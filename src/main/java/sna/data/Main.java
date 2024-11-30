e40c5b26b9a33564dda7790f56c9b648068e2118
soldatstar <h.logan.h@hotmail.com>
2024-11-30 11:23:35 +0100

    sample testing

--- src/main/java/sna/data/Main.java
+++ src/main/java/sna/data/Main.java
@@ -1,36 +1,90 @@
 package sna.data;
 
+import it.uniroma1.dis.wsngroup.gexf4j.core.EdgeType;
+import it.uniroma1.dis.wsngroup.gexf4j.core.Gexf;
+import it.uniroma1.dis.wsngroup.gexf4j.core.Graph;
+import it.uniroma1.dis.wsngroup.gexf4j.core.Mode;
+import it.uniroma1.dis.wsngroup.gexf4j.core.Node;
+import it.uniroma1.dis.wsngroup.gexf4j.core.impl.GexfImpl;
+import it.uniroma1.dis.wsngroup.gexf4j.core.impl.StaxGraphWriter;
 import net.sandrohc.jikan.Jikan;
 import net.sandrohc.jikan.exception.JikanQueryException;
 import net.sandrohc.jikan.model.anime.Anime;
+import net.sandrohc.jikan.model.common.Producer;
 import reactor.core.publisher.Flux;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.Writer;
+import java.util.Calendar;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Main {
 
     public static void main(String[] args) throws JikanQueryException {
-        System.out.println("Hello World");
+        System.out.println("Fetching anime and producers...");
+
         Jikan jikan = new Jikan();
+        Gexf gexf = new GexfImpl();
+        Calendar date = Calendar.getInstance();
 
-        long startTime = System.currentTimeMillis();
+        gexf.getMetadata()
+                .setLastModified(date.getTime())
+                .setCreator("Anime Network")
+                .setDescription("Producers to Anime Network");
+        gexf.setVisualization(true);
 
-        Anime anime = jikan.query().anime().get(1)
-                .execute()
-                .block();
+        Graph graph = gexf.getGraph();
+        graph.setDefaultEdgeType(EdgeType.UNDIRECTED).setMode(Mode.STATIC);
 
-        Flux<Anime> top100 = Flux.range(1, 4)
-                .concatMap(i -> {
-                    System.out.println("Fetching page " + i);
+        Set<String> nodeIds = new HashSet<>();
+        //Sample code for creating a two node network with Producer -> Anime relationship
+        Flux<Anime> topAnime = Flux.range(1, 4)
+                .concatMap(page -> {
+                    System.out.println("Fetching page " + page);
                     try {
-                        return jikan.query().anime().top().limit(25).page(i).execute();
+                        return jikan.query().anime().top().limit(25).page(page).execute();
                     } catch (JikanQueryException e) {
                         throw new RuntimeException(e);
                     }
                 });
 
-        top100.collectList().block().forEach(System.out::println);
+        topAnime.collectList().block().forEach(anime -> {
+            String animeId = "anime_" + anime.getMalId();
+            if (!nodeIds.contains(animeId)) {
+                Node animeNode = graph.createNode(animeId);
+                animeNode.setLabel(anime.getTitle());
+                nodeIds.add(animeId);
+            }
 
-        long endTime = System.currentTimeMillis();
-        long duration = endTime - startTime;
-        System.out.println("Total time taken: " + duration + " milliseconds");
+            for (Producer producer : anime.getProducers()) {
+                String producerId = "producer_" + producer.getMalId();
+                Node producerNode;
+                if (!nodeIds.contains(producerId)) {
+                    producerNode = graph.createNode(producerId);
+                    producerNode.setLabel(producer.getName());
+                    nodeIds.add(producerId);
+                } else {
+                    producerNode = graph.getNode(producerId);
+                }
+
+                Node animeNode = graph.getNode(animeId);
+                if (animeNode != null) {
+                    producerNode.connectTo(animeNode);
+                }
+            }
+
+        });
+
+        try {
+            StaxGraphWriter graphWriter = new StaxGraphWriter();
+            File outputFile = new File("anime_producer_network.gexf");
+            Writer out = new FileWriter(outputFile, false);
+            graphWriter.writeToStream(gexf, out, "UTF-8");
+            System.out.println("Graph saved to: " + outputFile.getAbsolutePath());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
