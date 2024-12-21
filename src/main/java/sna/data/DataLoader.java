3dfba2270d3846e1646c8bf72d8cecfda58db595
soldatstar <h.logan.h@hotmail.com>
2024-12-21 12:31:35 +0100

    clean node generation

--- src/main/java/sna/data/DataLoader.java
+++ src/main/java/sna/data/DataLoader.java
@@ -1,14 +1,14 @@
 package sna.data;
 
 import java.io.IOException;
-import java.util.Comparator;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.*;
 
 import net.sandrohc.jikan.model.MalEntity;
 import net.sandrohc.jikan.model.anime.AnimeType;
 import net.sandrohc.jikan.model.common.Producer;
+import net.sandrohc.jikan.model.person.PersonSimple;
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVPrinter;
 
@@ -23,29 +23,37 @@ import reactor.core.publisher.Flux;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.util.stream.Collectors;
+import java.io.File;
 
 public class DataLoader {
 
     private Jikan jikan;
     private List<Anime> animeList;
+    private List<Studio> studioList = new ArrayList<>();
+    Map<Anime, List<PersonSimple>> voiceActorsMap = new HashMap<>();
 
     public DataLoader() { jikan = new Jikan();}
 
     public static void main(String[] args) {
         long startTime = System.currentTimeMillis();
-        DataLoader loader = new DataLoader();
-        List<Anime> animes = loader.getRawData();
-        String[] animeHeader = {"id", "label", "name", "score", "rank"};
 
+        CleanDataDir();
+
+        DataLoader loader = new DataLoader();
+
+        loader.fetchRawData();
+        loader.mapAnimeActor(loader.animeList);
         // Speichern der Knoten
-        loader.saveNodes("./data/Anime_Nodes.csv", animes, "Anime", animeHeader);
+        String[] animeHeader = {"id", "label", "name", "score", "rank"};
+        String[] studioHeader = {"id", "label", "name"};
+        loader.saveNodes("./data/Anime_Nodes.csv", loader.animeList, "Anime", animeHeader);
+        loader.saveNodes("./data/Studio_Nodes.csv", loader.studioList, "Studio", studioHeader);
 
         // Speichern der Kanten
         loader.saveAnimeEdges("./data/Anime_Producers_Edges.csv", "producers");
         loader.saveAnimeEdges("./data/Anime_Studios_Edges.csv", "studios");
         // Abrufen der Synchronsprecher geht sehr lange, da für jeden Anime die Synchronsprecher abgerufen werden müssen
         // Zum testen kann die Zeile ".take(1000)" in der Methode "getRawData()"  auf ".take(10)" geändert werden
-        loader.loadVoiceActorEdges("./data/VoiceActors_Edges.csv");
 
         long endTime = System.currentTimeMillis();
         long duration = endTime - startTime;
@@ -53,14 +61,14 @@ public class DataLoader {
     }
 
 
-    private List<Anime> getRawData() {
+
+    private  void fetchRawData() {
         if (animeList != null && !animeList.isEmpty()) {
-            return animeList;
+            return;
         }
         System.out.println("Fetching anime data...");
         animeList = Flux.range(1, Integer.MAX_VALUE)
                 .concatMap(page -> {
-                    System.out.println("Fetching page " + page);
                     try {
                         return jikan.query().anime().top().limit(25).page(page).execute();
                     } catch (JikanQueryException e) {
@@ -68,20 +76,23 @@ public class DataLoader {
                     }
                 })
                 .filter(anime -> anime.getType() == AnimeType.TV || anime.getType() == AnimeType.MOVIE)
-                .take(1000)// ändern auf 10 für schnelleres Testen
+                .take(10)// ändern auf 10 für schnelleres Testen
                 .collectList()
                 .block();
-        return animeList;
+        animeList.forEach(anime -> {
+            studioList.addAll(anime.getStudios());
+        });
     }
 
 
     private void saveNodes(String writerPath, List<?> datapoints, String label, String[] header) {
         CSVFormat format = CSVFormat.DEFAULT.builder().setHeader(header).build();
-
+        boolean fileExists = Files.exists(Paths.get(writerPath));
         System.out.println("Saving" + label + "nodes...");
 
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter(writerPath));
-            final CSVPrinter printer = new CSVPrinter(writer, format)) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(writerPath, fileExists));
+            final CSVPrinter printer = fileExists ? new CSVPrinter(writer, CSVFormat.DEFAULT) : new CSVPrinter(writer, format)) {
+
             int size = datapoints.size();
             String progress = "|";
             int counter = 0;
@@ -89,17 +100,21 @@ public class DataLoader {
             for (Object data : datapoints) {
                 // Display the progress
                 int percentage = (int) ((counter / (double) size) * 100) + 1;
-                if (counter % (size / 100) == 0 || counter == size - 1) {
+                if (counter %  (size < 100 ? size : (size / 100)) == 0 || counter == size - 1) {
                     progress += "=";
                     System.out.print("\r" + progress + "| " + percentage + "%");
-    
+
                 }
 
-                Object[] record;
+                Object[] record = null;
                 
-                if (data instanceof Anime) {
-                    Anime anime = (Anime) data;
+                if (data instanceof Anime anime) {
                     record = processAnime(anime);
+                }
+                else if (data instanceof PersonSimple voiceActor) {
+                    record = new String[]{String.valueOf(voiceActor.getMalId()), "Voice-Actor", voiceActor.getName()};
+                }else if (data instanceof Studio studio) {
+                    record = new String[]{String.valueOf(studio.getMalId()), "Studio", studio.getName()};
                 }
 
                 printer.printRecord((Object[]) record);
@@ -126,12 +141,13 @@ public class DataLoader {
         return record;
     }
 
+
     private void saveAnimeEdges(String writerPath, String relationType) {
         String[] header = {"source", "target", "type"};
         CSVFormat format = CSVFormat.DEFAULT.builder().setHeader(header).build();
 
         System.out.println("Saving anime edges...");
-        List<String[]> edges = getRawData().stream()
+        List<String[]> edges = animeList.stream()
                 .flatMap(anime -> {
                     int animeId = anime.getMalId();
                     List<String> relatedItems = relationType.equals("studios")
@@ -156,52 +172,43 @@ public class DataLoader {
         }
     }
 
-    public void loadVoiceActorEdges(String writerPath) {
-        String[] header = {"source", "target", "type"};
-        CSVFormat format = CSVFormat.DEFAULT.builder().setHeader(header).build();
+    private void mapAnimeActor(List<Anime> anime){
+        System.out.println("Lade Voice Actor-Verbindungen...");
+        String[] voiceActorHeader = {"id", "label", "name"};
+        for (Anime currentAnime : anime) {
+            String animeTitle = currentAnime.getTitle();
+            System.out.println("Abrufen von Synchronsprechern für: " + animeTitle);
+            try {
+                Flux<CharacterBasic> current = jikan.query().anime().characters(currentAnime.malId).execute();
 
-        System.out.println("Lade Daten in das Programm...");
-        List<Anime> topAnime = getRawData();
-        topAnime.sort(Comparator.comparingInt(MalEntity::getMalId));
-        List<Integer> malIds = topAnime.stream().map(Anime::getMalId).toList();
-        List<String> titles = topAnime.stream().map(Anime::getTitle).toList();
+                List<PersonSimple> voiceActors = current.toStream()
+                        .filter(character -> character.getRole().equals(CharacterRole.MAIN))
+                        .flatMap(character -> character.getVoiceActors().stream())
+                        .filter(actor -> actor.getLanguage().equals("Japanese"))
+                        .map(actor -> actor.getPerson())
+                        .toList();
+                saveNodes("./data/VoiceActor_Nodes.csv"  , voiceActors, "Voice-Actor", voiceActorHeader);
+                voiceActorsMap.put(currentAnime, voiceActors);
 
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter(writerPath));
-             CSVPrinter printer = new CSVPrinter(writer, format)) {
-
-            System.out.println("Lade Voice Actor-Verbindungen...");
-            for (int i = 0; i < malIds.size(); i++) {
-                String animeTitle = titles.get(i);
-                System.out.println("Abrufen von Synchronsprechern für: " + animeTitle);
-                try {
-                    Flux<CharacterBasic> current = jikan.query().anime().characters(malIds.get(i)).execute();
-
-                    List<String> voiceActors = current.toStream()
-                            .filter(character -> character.getRole().equals(CharacterRole.MAIN))
-                            .flatMap(character -> character.getVoiceActors().stream())
-                            .filter(actor -> actor.getLanguage().equals("Japanese"))
-                            .map(actor -> actor.getPerson().getName())
-                            .toList();
-
-                    for (String voiceActor : voiceActors) {
-                        // Schreibe jede Verbindung in die CSV-Datei
-                        printer.printRecord(animeTitle, voiceActor, "undirected");
-                    }
-
-                } catch (JikanQueryException e) {
-                    System.err.println("Fehler beim Abrufen von Voice Actors für " + animeTitle + ": " + e.getMessage());
-                }
-
-                if (i % 10 == 0) {
-                    System.out.println("Fortschritt: " + i + "/" + malIds.size());
-                }
+            } catch (JikanQueryException e) {
+                System.err.println("Fehler beim Abrufen von Voice Actors für " + animeTitle + ": " + e.getMessage());
             }
+        }
 
-            printer.flush();
-            System.out.println("Voice Actor-Edges CSV wurde erfolgreich erstellt: " + writerPath);
 
+    }
+
+
+    // Löscht alle Dateien im data-Ordner, da die neuen rows im VoiceActor-File appended
+    private static void CleanDataDir() {
+        try {
+            System.out.println("Bereite Datenordner vor...");
+            Files.walk(Paths.get("./data"))
+                    .filter(Files::isRegularFile)
+                    .map(java.nio.file.Path::toFile)
+                    .forEach(File::delete);
         } catch (IOException e) {
-            System.err.println("Fehler beim Schreiben der CSV-Datei: " + e.getMessage());
+            e.printStackTrace();
         }
     }
 }
